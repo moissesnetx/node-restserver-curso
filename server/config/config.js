791a7647f3f2def9d3e3f1cb4120ee82f9318223
moises <moissesnetx@gmail.com>
2020-07-22 22:39:57 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://bdmmr:6naWXmD3XhRFJHd8@cursonodem.5pgrl.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
